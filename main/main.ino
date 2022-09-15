12e4285316b6a69b13fb67b5957f1ad405905cd6
SageScroll18144 <lipe.fssantos063@gmail.com>
2022-09-15 11:24:16 -0300

    Implementando as funções de movimentação

--- main/main.ino
+++ main/main.ino
@@ -2,6 +2,7 @@ const int pinos_ph[4] = {};
 
 void setup() {
   for(int i=0;i<sizeof(pinos_ph)/sizeof(int);i++) pinMode(pinos_ph[i], OUTPUT);
+  Serial.begin(9600);//FOR DEBUG
 }
 
 void loop() {
--- main/ponte_h.ino
+++ main/ponte_h.ino
@@ -1,1 +1,34 @@
-void set_Mode(int motor, char sense){} 
+void setMode(int motor, char sense){
+  if(sense == 'F'){
+    digitalWrite(pinos_ph[(2*motor)], HIGH);
+    digitalWrite(pinos_ph[(2*motor)+1], LOW);
+  }else if(sense == 'B'){
+    digitalWrite(pinos_ph[(2*motor)], LOW);
+    digitalWrite(pinos_ph[(2*motor)+1], HIGH);
+  }else if(sense == 'S'){
+    digitalWrite(pinos_ph[(2*motor)], LOW);
+    digitalWrite(pinos_ph[(2*motor)+1], LOW);
+  }else{
+    Serial.println("*sense was incorrect value!*");
+  }
+}
+void forward(){
+  setMode(0, 'F');
+  setMode(1, 'F');
+}
+void backward(){
+  setMode(0, 'B');
+  setMode(1, 'B');
+}
+void stop(){
+  setMode(0, 'S');
+  setMode(1, 'S');
+}
+void turnLeft(){
+  setMode(0, 'F');
+  setMode(1, 'B');
+}
+void turnRight(){
+  setMode(0, 'B');
+  setMode(1, 'F');
+}
