d1cd546d57fc0627e75c16c8150a7095b7ea3026
SageScroll18144 <lipe.fssantos063@gmail.com>
2022-09-20 18:24:40 -0300

    setando as variaveis

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,12 @@
-const int pinos_ph[] = {};
+const int pinos_ph[] = {6, 7, 5, 4};
+const int pinos_v[] = {9, 3};
 const int ultrassonic = 0;
-const int sensor_ir = 0;
+const int sensor_ir = 2;
 
 void setup() {
   for(int i=0;i<sizeof(pinos_ph)/sizeof(int);i++) pinMode(pinos_ph[i], OUTPUT);
+  setVelocity(0, 255);
+  setVelocity(1, 255);
   Serial.begin(9600);//FOR DEBUG
 }
 
