1365426cd69fb52586aa7ae5ea5a9df77fee1a4c
arthurmarsar0 <108816696+arthurmarsar0@users.noreply.github.com>
2022-10-11 15:41:25 -0300

    Update algorithm.ino

--- main/algorithm.ino
+++ main/algorithm.ino
@@ -5,45 +5,67 @@ double speed = 27.5;
 int distanciaPercorrida;
 
 void algorithm(){
- if((ultrassonicRead() <= distancia_segura) && (ultrassonicRead() >= 5)){
-  //digitalWrite(LED, HIGH);
-  //desvio
-  stop();
-  tgAngle = obstWidth / ultrassonicRead();
-  Angle_rad = atan(tgAngle);
-  Angle_degree = (Angle_rad * 180) / PI; 
-  setVelocity(L, 60);
-  setVelocity(R, 60);
-  turnLeft();
-  // periodLeft = calcular o tempo que o carrinho leva para dar uma volta completa em torno do própio eixo (pela esquerda)
-  spinTimeLeft = (Angle_rad * periodLeft) / (2 * PI);
-  delay(spinTimeLeft);
-  stop();
-  setVelocity(L, 60);
-  setVelocity(R, 60);
-  forward();
-  distanciaPercorrida = ((obstLength/2) + distancia_segura) / cos(Angle_degree);
-  // speed0 = calcular a velocidade em linha reta do carro em cm/s
-  forwardTime = distanciaPercorrida / speed;
-  delay(forwardTime);
-  stop();
-  // periodRight = calcular o tempo que o carrinho leva para dar uma volta completa em torno do própio eixo (pela direita)
-  spinTimeRight = (Angle_rad * periodRight) / PI;
-  delay(spinTimeRight);
-  stop();
-  setVelocity(L, 60);
-  setVelocity(R, 60);
-  forward();
- }else{
-    //digitalWrite(LED, LOW);
-    setVelocity(L, 60);
+ const int timeToTurnLeft = 250;
+ const int timeToTurn90Right = 900;
+ const int timeToGoForward = 1750; 
+  if((ultrassonicRead() <= distancia_segura) && flag == 1){
+   while(1){
+     setVelocity(L, 255);
+    setVelocity(R, 255);
+    turnLeftMiddleRobot();
+    play_music();
+    digitalWrite(20, HIGH);
+     digitalWrite(21, LOW);
+    delay(400);
+    digitalWrite(20, LOW);
+    digitalWrite(21, HIGH);
+    flag = 0;
+   }
+  }
+  else if((ultrassonicRead() <= distancia_segura) && flag == 0){
+        digitalWrite(20, HIGH);
+        stop();
+        delay(500);
+        //vira 90 graus para a esquerda
+        setVelocity(L, 60);
+        setVelocity(R, 66);
+        turnLeftMiddleRobot();
+        delay(timeToTurnLeft);
+        stop();
+        //anda durante 5 segundos para a frente
+        setVelocity(L, 50);
+        setVelocity(R, 55);
+        forward();
+        delay(timeToGoForward);
+        //vira 90 graus para a direita
+        digitalWrite(20,LOW);
+        stop();
+        delay(1000);
+        digitalWrite(21, HIGH);
+        setVelocity(L, 62);
+        setVelocity(R, 70);
+        turnRight();
+        delay(timeToTurn90Right);
+        stop();
+        //anda durante 5 segundos para a frente
+        setVelocity(L, 50);
+        setVelocity(R, 55);
+        forward();
+        //vira 90 graus para a direita
+        while(!stayOnBlackLine());
+        sen = 0; //Ajeita o valor do sentido (o desvio é feito pra esquerda)
+        digitalWrite(20,LOW);
+        digitalWrite(21, LOW);
+        flag = 1;
+    }
+  else{
+    setVelocity(L, 60);//mudanca
     setVelocity(R, 60);
-    if(!stayOnBlackLine()) {
-      while(!stayOnBlackLine());
+    if(stayOnBlackLine()) {
+      while(stayOnBlackLine());
       sen = !sen;
     }
     if(sen) turnLeft();
     else turnRight();
-
- }
+  }
 }
